46508ac2dfbbb3fdf9dc0d4f8b42a3d5456924f7
falex2 <francisco.alexander.arriaga@gmail.com>
2023-07-18 08:53:17 -0600

    Unicafe part1

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -1,55 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
-const Header = (props) => {
-  return <h1>{props.course}</h1>;
-};
-
-const Content = (props) => {
-  return (
-    <div>
-      <p>
-        {props.part1.name} {props.part1.exercises}
-      </p>
-      <p>
-        {props.part2.name} {props.part2.exercises}
-      </p>
-      <p>
-        {props.part3.name} {props.part3.exercises}
-      </p>
-    </div>
-  );
-};
-
-const Total = (props) => {
-  return (
-    <p>
-      Number of exercises{' '}
-      {props.part1.exercises + props.part2.exercises + props.part3.exercises}
-    </p>
-  );
-};
-
 const App = () => {
-  const course = 'Half Stack application development';
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10,
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const [totalComments, setTotalComments] = useState(0);
+
+  // Function to handle the click of each button
+  const handleButtonClick = (type) => {
+    switch (type) {
+      case 'good':
+        setGood(good + 1);
+        break;
+      case 'neutral':
+        setNeutral(neutral + 1);
+        break;
+      case 'bad':
+        setBad(bad + 1);
+        break;
+      default:
+        break;
+    }
+    setTotalComments(totalComments + 1);
   };
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7,
+
+  // Calculate the average score
+  const calculateAverageScore = () => {
+    if (totalComments === 0) return 0;
+    return (good - bad) / totalComments;
   };
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14,
+
+  // Calculate the percentage of positive comments
+  const calculatePositivePercentage = () => {
+    if (totalComments === 0) return 0;
+    return (good / totalComments) * 100;
   };
 
   return (
     <div>
-      <Header course={course} />
-      <Content part1={part1} part2={part2} part3={part3} />
-      <Total part1={part1} part2={part2} part3={part3} />
+      <h1>Feedback App</h1>
+      <div>
+        <button onClick={() => handleButtonClick('good')}>Good</button>
+        <button onClick={() => handleButtonClick('neutral')}>Neutral</button>
+        <button onClick={() => handleButtonClick('bad')}>Bad</button>
+      </div>
+      <h2>Statistics</h2>
+      <p>Total comments: {totalComments}</p>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
+      <p>Average score: {calculateAverageScore()}</p>
+      <p>Positive comments: {calculatePositivePercentage()}%</p>
     </div>
   );
 };
